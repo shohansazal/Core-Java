ba3819b6eb56d8be6ecac9579a99f0969f35a9f3
shohansazal <shohansazal@gmail.com>
2017-09-26 23:39:44 +0600

    Variable Data Types

---
+++ VariableDataTypes-03.java
@@ -0,0 +1,30 @@
+package com.shohan;
+
+public class VariableDataTypes {
+
+//    Variables: 
+//    local - declared inside the method 
+//    instance - declared inside the class but outside the method
+//    static - declared as static. It cannot be local.
+//   -------------- Example------------
+    /*   
+class Variable{  
+int  m = 50;            //instance variable  - / global variable / non static variable
+static int n = 100;    //static variable  
+void method(){  
+int o = 90; }                //local variable  
+}  
+     */
+//    =========================
+//    Data Types:
+//    Primitive -> 
+//            Boolean ->> boolean
+//            Numeric ->>
+//                                  Character ->>  char
+//                                  Integral ->>
+//                                                        Integer : byte , short , int , long
+//                                                        floating-point : float , double
+//    Non-Primitive ->
+//                                String
+//                                Array
+}
