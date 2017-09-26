a732640f09f2d07ee7b92c5ca2ba0c3a33262a3e
shohansazal <shohansazal@gmail.com>
2017-09-26 23:45:02 +0600

    Oops Concept

---
+++ OopsConcept-08.java
@@ -0,0 +1,26 @@
+package com.shohan;
+
+public class OopsConcept {
+    
+//OOPs Concepts:  
+//        Simula is considered as the first object-oriented programming language.
+//        Smalltalk is considered as the first truly object-oriented programming language.
+    
+//    -------------------------------------------------------------------------------------------------------------
+    
+//OOPs (Object Oriented Programming System):
+    
+//Object : Any entity that has state and behavior is known as an object. For example: chair, pen, table, keyboard, bike etc.
+    
+//Class : Collection of objects is called class. It is a logical entity.
+    
+//Inheritance : When one object acquires all the properties and behaviours of parent object, known as inheritance. It provides code reusability.
+    
+//Polymorphism : When one task is performed by different ways, known as polymorphism.
+    
+//Abstraction : Hiding internal details and showing functionality is known as abstraction. For example: phone call, we don't know the internal processing.
+                           //in java, we use abstract class and interface to achieve abstraction.
+    
+//Encapsulation : Binding (or wrapping) code and data together into a single unit is known as encapsulation. 
+                               //For example: capsule, it is wrapped with different medicines.
+}
