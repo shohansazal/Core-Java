c78ad9051560bb5bd69b3a45da8a5b47cbb306f7
shohansazal <shohansazal@gmail.com>
2017-09-26 23:44:06 +0600

    Java Comment

---
+++ JavaComment-07.java
@@ -0,0 +1,24 @@
+package com.shohan;
+
+public class JavaComment {
+//3 types of comments in java:
+    
+    //Single Line Comment :
+            //This is single line comment  
+    
+    //Multi Line Comment :
+            /* 
+            This  
+            is  
+            multi line  
+            comment 
+            */  
+    
+    //Documentation Comment :
+            /** 
+            This  
+            is  
+            documentation  
+            comment 
+            */
+}
