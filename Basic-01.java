1bb1e5ac04058613053034f4f63485ab20b1b295
shohansazal <shohansazal@gmail.com>
2017-09-26 19:18:11 +0600

    Basic

---
+++ Basic-01.java
@@ -0,0 +1,51 @@
+
+package com.shohan;
+
+public class Basic {
+    
+//    JDK - Java development kit
+//    JVM - Java virtual machine
+//    JRE - Java runtime environment
+    
+//    ============================
+//    Java SE basic tools :
+//    javac - compiler
+//    java - Launcher
+//    jdb - java debugger
+//    javadoc - API document generator
+//    jar - java Archive
+    
+//    ===============================
+//    Standard Libraries:
+//    java.lang
+//    java.util
+//    java.io
+//    java.math
+//    java.text
+//    javax.crypto
+//    java.net
+//    java.sql
+//    javax.swing
+//    javax.xml.parsers
+    
+//    =====================================
+//    JVM implementation, Application & OS:
+//    -> Hardware Platform -> OS -> JVM -> Java Technology Application
+    
+//    ========================================
+//    Operation of the JRE:
+//    TestGreting.java
+//          javac
+//    TestGreting.class
+//                   ( Load from hard disk,network or other source )
+//          Runtime
+//    class loader
+//    Bytecode verfier
+//    Hotspot Execution Engine
+//    Hardware
+            
+                        
+            
+    
+           
+}
