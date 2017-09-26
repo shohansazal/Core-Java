084f737175888c1a96367dba84ab09c459c169e0
shohansazal <shohansazal@gmail.com>
2017-09-26 23:37:57 +0600

    Print

---
+++ Print-02.java
@@ -0,0 +1,26 @@
+
+package com.shohan;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("Hello Java Programmer");
+    }
+    
+//    Access Modifiers:
+//    private , default , protected , public 
+    
+//    ==========================
+    
+//    Non Access Modifiers:
+//    static , final , abstract , synchronized , volatile
+    
+//    ============================
+    
+//    Void:
+//    void method does not return any value
+    
+//    ==============================
+    
+//    String [] args:
+//    string type array
+}
