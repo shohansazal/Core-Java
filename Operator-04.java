492ab958b7f5175a022f63f40aea010117de8a11
shohansazal <shohansazal@gmail.com>
2017-09-26 23:40:56 +0600

    Operators

---
+++ Operator-04.java
@@ -0,0 +1,35 @@
+
+package com.shohan;
+
+public class Operator {
+//    -----Operators-----
+//    Unary Operator:
+//    prefix -> ++expr , --expr
+//    postfix -> expr++ , expr--
+//    ---------------------------------------
+//    Arithmetic Operator:	
+//    + ,  -  , * ,  / ,  %
+//    -------------------------------------------
+//Shift Operator:
+//    << , >> , >>>
+//    --------------------------------------------
+//    Relational Operator:
+//    comparison ->    <  , > , <=  , >= , instanceof
+//    equality -> == , !=
+//    --------------------------------------------------
+//    Bitwise Operator:
+//    AND -> &
+//    bitwise exclusive OR -> ^
+//    bitwise inclusive OR -> |
+//    -------------------------------------------------
+//    Logical Operator:
+//    logical AND -> &&
+//    logical OR ->  ||
+//    --------------------------------------------
+//    Ternary Operator:
+//    ternary -> ? , :
+//    -------------------------------------
+//    Assignment Operator:
+//    assignment -> = , += , -=  , *=  , /= , %= , &= , ^= , |= , <<= , >>= , >>>=
+    
+}
